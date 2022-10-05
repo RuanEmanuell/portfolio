64014b9a9d1de94eb3ae8aa7432fc654fcff3694
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-10-05 19:04:06 -0300

    Removendo dark theme

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0b45ecd3a6e81c69182007b5341a09e8",
-"/": "0b45ecd3a6e81c69182007b5341a09e8",
-"main.dart.js": "7bdef4ddf6831563cc74e7ef99b6537c",
+"index.html": "440e169291051878e5e9c8ff0633e956",
+"/": "440e169291051878e5e9c8ff0633e956",
+"main.dart.js": "20deecadb1f9fd02c1beb244a3bb259e",
 "manifest.json": "a755f7277e1697f92f71f2f48e8781e4",
 "version.json": "396c903be24e05680276e5d88d453065"
 };
